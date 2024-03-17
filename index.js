e99ee93e76cddfd60235c3f66a1bbe8b19728e95
Andrey Lukoshkin <lukoshkin.andrey@gmail.com>
2024-03-17 13:20:39 +0200

    new buttons, adaptive styles

--- index.js
+++ index.js
@@ -46,6 +46,23 @@ getData('https://jsonplaceholder.typicode.com/posts')
         enabled: true,
         onlyInViewport: false,
       },
+
+      breakpoints: {
+        // when window width is >= 320px
+        1: {
+          slidesPerView: 1,
+          spaceBetween: 20,
+        },
+        768: {
+          slidesPerView: 2,
+          spaceBetween: 30,
+        },
+        // when window width is >= 640px
+        1440: {
+          slidesPerView: 4,
+          spaceBetween: 40,
+        },
+      },
     })
   })
   .catch((error) => {
